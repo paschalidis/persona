e312050a24b7d880e63e2b1b1a2b47c9c000e51a
Pavlos Paschalidis <pavlos.paschalidis@upiria.com>
2024-09-03 02:39:54 +0300

    postman collection

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+Route::get('/', function () {
+    return view('welcome');
+});
 
-//Route::get('/test', function () {
-//    return csrf_token();
-//});
+Route::get('/test', function () {
+    return 'Hello world';
+});
 
 Route::post('/submission', [\App\Http\Controllers\SubmissionController::class, 'create']);
